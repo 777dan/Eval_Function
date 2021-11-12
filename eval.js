c6d2d3bdc82f1d9f83156b06f41ffeed8d032a13
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-11-12 20:42:17 +0200

    Add files via upload

---
+++ eval.js
@@ -0,0 +1,34 @@
+function printTable(from, to, step, func) {
+    if (func === undefined) {
+        return;
+    }
+    from = from;
+    to = to;
+    step = step || 1;
+    document.write("<table border='1'>");
+    for (var x = from; x <= to; x += step) {
+        document.write("<tr>");
+        document.write("<td>" + x + "</td>");
+        document.write("<td>" + func(x) + "</td>");
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
+
+function quadratic(x) {
+    return x * x;
+}
+
+function cube(x) {
+    return x * x * x;
+}
+
+let expr = prompt("input expression for function");
+let f;
+f = function (x) { return x * expr; };
+eval(f);
+//alert(functionStr);
+// let ff = [quadratic, cube];
+printTable(-4, 4, 1, f);
+
+// let f1 = new Function("x", "return " + expr);
